ffe3094c5535a8c3b2b87b4cbf5251f214b75bba
Lucas Souza <lucasas@gmail.com>
2017-05-22 09:46:54 -0300

    Initial commit

---
+++ lib/fluent/plugin/snowplow.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/snowplow/version"
+
+module Fluent
+  module Plugin
+    module Snowplow
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/snowplow/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Snowplow
+      VERSION = "0.1.0"
+    end
+  end
+end
